ba0b9a5a82f174f5753ce50e56c2f7868a4d7132
echaoo <1299529160@qq.com>
2016-03-21 17:27:51 +0800

    init

---
+++ src/main/java/cn/zhangmengke/noticeboard/NoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package cn.zhangmengke.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardApplication.class, args);
+	}
+}
