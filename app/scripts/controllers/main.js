619bc4db5fffbc77e735f2f5d8f0acee3cf66fb9
skyADMIN <546485299@qq.com>
2016-07-10 22:48:29 +0800

    init project by yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noticeBoardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the noticeBoardApp
+ */
+angular.module('noticeBoardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
