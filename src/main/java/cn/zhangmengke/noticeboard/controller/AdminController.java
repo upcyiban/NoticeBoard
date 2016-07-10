6e254abc7e6fd055b661ae5f522291b8433bd178
skyADMIN <546485299@qq.com>
2016-07-10 22:17:01 +0800

    clean project

--- src/main/java/cn/zhangmengke/noticeboard/controller/AdminController.java
+++
@@ -1,8 +0,0 @@
-package cn.zhangmengke.noticeboard.controller;
-
-/**
- * Created by echao on 2016/3/25.
- */
-public class AdminController
-{
-}
--- src/main/java/cn/zhangmengke/noticeboard/model/NoticeListDao.java
+++
@@ -1,9 +0,0 @@
-package cn.zhangmengke.noticeboard.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by echao on 2016/3/21.
- */
-public interface NoticeListDao extends CrudRepository<NoticeList,Long> {
-}
