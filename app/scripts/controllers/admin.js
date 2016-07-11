7221e6eb5596be50563d067b4a850739d30410f0
skyADMIN <546485299@qq.com>
2016-07-11 22:52:28 +0800

    admin page add new function

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -8,10 +8,25 @@
  * Controller of the noticeBoardApp
  */
 angular.module('noticeBoardApp')
-  .controller('AdminCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AdminCtrl', function ($http, $location, $scope) {
+
+    $http.get(apiURL + 'isadmin').then(function (response){
+      if (response.data == 0) {
+        $location.url('/')
+      }
+    });
+
+    $scope.postNew = function () {
+      var data = $.param({
+        title: $scope.title,
+        message: $scope.message,
+        tag: 'default'
+      });
+      $http.post(apiURL + 'postnew', data, postconfig).then(function (response) {
+        if (response.data.code == 0){
+          console.log("success");
+        }
+      })
+    }
+
   });
